08d936910f62c9487507d1451a90f291b846c4f2
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2022-01-29 07:33:14 +0800

    feat: default title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { MainLayout } from 'layout/main';
 import { theme } from 'theme';
@@ -8,6 +9,10 @@ import { theme } from 'theme';
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>FarmTogether</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
